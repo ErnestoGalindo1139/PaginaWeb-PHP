37354ed72186e199e00a75b42d16d361c6479f84
ErnestoGalindo1139 <galindohernandezernesto@gmail.com>
2024-02-07 14:10:19 -0700

    Creacion de pagina web con php

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin</title>
+    <link rel="stylesheet" href="./css/estilos.css">
+</head>
+<body>
+    
+    <h1 class="titulo">Admin de tienda en linea</h1>
+    <table class="tabla">
+        <thead>
+            <tr>
+                <th>Imagen</th>
+                <th>Descripcion</th>
+                <th>Precio</th>
+                <th>Cantidad</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>M</td>
+                <td>O</td>
+                <td>M</td>
+                <td>O</td>
+            </tr>
+            <tr>
+                <td>M</td>
+                <td>O</td>
+                <td>M</td>
+                <td>O</td>
+            </tr>
+        </tbody>
+    </table>
+
+</body>
+</html>
